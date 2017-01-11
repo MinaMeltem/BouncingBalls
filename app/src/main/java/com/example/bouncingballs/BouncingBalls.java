4898ddcd3791e95ae06ee0d59a2fdff78473200a
MinaMeltem <meltem@gmail.com>
2017-01-11 13:46:38 -0500

    Fixed method issue in TypedArray

--- app/src/main/java/com/example/bouncingballs/BouncingBalls.java
+++ app/src/main/java/com/example/bouncingballs/BouncingBalls.java
@@ -28,21 +28,20 @@ public class BouncingBalls extends View {
 
     public BouncingBalls(Context context) {
         super(context);
-        init(null, 0);
+
     }
 
     public BouncingBalls(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        TypedArray a = context.getTheme()
-                .obtainStyledAttributes(attrs, R.styleable.BouncingBalls, 0, 0);
-
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.BouncingBalls, 0, 0);
         try {
-            circleXPosition = a.getInteger(R.styleable.BouncingBalls_circleXPosition, 0);
-            circleYPosition = a.getInteger(R.styleable.BouncingBalls_circleYPosition, 0);
-            circleXSpeed = a.getInteger(R.styleable.BouncingBalls_circleXSpeed, 0);
-            circleYSpeed = a.getInteger(R.styleable.BouncingBalls_circleYSpeed, 0);
-            circleRadius = a.getInteger(R.styleable.BouncingBalls_circleRadius, 10);
+            circleRadius = a.getDimensionPixelSize(R.styleable.BouncingBalls_circleRadius, 0);
+            circleXPosition = a.getDimensionPixelSize(R.styleable.BouncingBalls_circleXPosition, 0);
+            circleYPosition = a.getDimensionPixelSize(R.styleable.BouncingBalls_circleYPosition, 0);
+            circleXSpeed = a.getDimensionPixelSize(R.styleable.BouncingBalls_circleXSpeed, 0);
+            circleYSpeed = a.getDimensionPixelSize(R.styleable.BouncingBalls_circleYSpeed, 0);
+            circleRadius = a.getDimensionPixelSize(R.styleable.BouncingBalls_circleRadius, 10);
             circleColor = a.getColor(R.styleable.BouncingBalls_circleColor, Color.BLACK);
 
         } finally {
@@ -50,19 +49,16 @@ public class BouncingBalls extends View {
         }
     }
 
-
     public BouncingBalls(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(attrs, defStyleAttr);
-    }
 
-    private void init(AttributeSet attrs, int defStyleAttr) {
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        paint.setColor(circleColor);
         canvas.drawCircle(circleXPosition, circleYPosition, circleRadius, paint);
 
         //moving right - left
